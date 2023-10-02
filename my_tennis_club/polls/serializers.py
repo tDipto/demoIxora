66a9871770e98f6cf14d58f0b8a32885c0a5f52e
Dipto <tdipto@yahoo.com>
2023-10-02 11:27:56 +0600

    clean code

--- my_tennis_club/polls/serializers.py
+++ my_tennis_club/polls/serializers.py
@@ -30,44 +30,7 @@ class PriceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Price
         fields = ['user_price', 'user_id_foreign', 'product_id_foreign', 'location_id_foreign', 'time_id_foreign']
-        # depth = 1  # Include related fields up to a depth of 1
 
-
-
-# class UserRegistrationSerializer(serializers.Serializer):
-#     username = serializers.CharField(max_length=200)
-#     email = serializers.EmailField(max_length=200)
-#     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
-#     confirm_password = serializers.CharField(write_only=True, style={'input_type': 'password'})
-
-#     def validate(self, data):
-
-#         if data.get('password') != data.get('confirm_password'):
-#             raise serializers.ValidationError("Passwords do not match.")
-
-        
-#         if User.objects.filter(email=data.get('email')).exists():
-#             raise serializers.ValidationError("Email address is already in use.")
- 
-     
-#         try:
-#             password_validation.validate_password(data.get('password'))
-#         except exceptions.ValidationError as e:
-#             raise serializers.ValidationError(str(e))
-
-#         return data
-
-#     def create(self, validated_data):
-#         user = User.objects.create_user(
-#             username=validated_data['username'],
-#             email=validated_data['email'],
-#             password=validated_data['password']
-#         )
-#         return user
-
-
-from django.contrib.auth.models import User
-from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(write_only=True, required=True)
